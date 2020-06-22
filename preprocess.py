0ba413572140f3b408b8cdf64373a9fba6ed4af4
yklu0330 <yklu0330@gmail.com>
2020-06-22 17:41:07 +0800

    Update preprocessing: add word tokenize

--- preprocess.py
+++ preprocess.py
@@ -13,11 +13,13 @@ from tqdm import tqdm
 import ipdb
 
 
-def initText(file, text, tag, val_start, val_end, able, index, index_bound, fileLen, text_decode, tag_decode):
+def initText(file, text_char, tag_char, text_word, tag_word, val_start, val_end, able, index, index_bound, fileLen, text_decode, tag_decode):
     return {
         'file': file,
-        'text': text,
-        'tag': tag,
+        'text_char': text_char,
+        'tag_char': tag_char,
+        'text_word': text_word,
+        'tag_word': tag_word,
         'val_start': val_start,
         'val_end': val_end,
         'able': able,
@@ -35,7 +37,7 @@ def removeSpace(inputStr):
     return outputStr
 
 
-def textTokenize(text):
+def textCharTokenize(text):
     tokenizer_char = BertTokenizer.from_pretrained(
         config.bert_char, do_lower_case=True)
     text = list(text)
@@ -44,11 +46,43 @@ def textTokenize(text):
     return textToken
 
 
-def tagTokenize(tag):
+def tagCharTokenize(tag):
     tokenizer_char = BertTokenizer.from_pretrained(
         config.bert_char, do_lower_case=True)
     tag = list(tag)
     tagToken = tokenizer_char.encode(tag)
+    tagToken.pop(0)
+    tagToken = torch.tensor(tagToken)
+    return tagToken
+
+
+def textWordTokenize(text):
+    tokenizer_word = BertTokenizer.from_pretrained(
+        config.bert, do_lower_case=True)
+
+    words = []
+    parse_token = config.mecab.parse(text).split(' ')
+    parse_token.remove('\n')
+    for word in parse_token:
+        words.extend([word for i in range(len(word))])
+
+    textToken = tokenizer_word.encode(words)
+    textToken = torch.tensor(textToken)
+
+    return textToken
+
+
+def tagWordTokenize(tag):
+    tokenizer_word = BertTokenizer.from_pretrained(
+        config.bert, do_lower_case=True)
+
+    words = []
+    parse_token = config.mecab.parse(tag).split(' ')
+    parse_token.remove('\n')
+    for word in parse_token:
+        words.extend([word for i in range(len(word))])
+
+    tagToken = tokenizer_word.encode(words)
     tagToken.pop(0)
     tagToken = torch.tensor(tagToken)
     return tagToken
@@ -90,7 +124,17 @@ def setText(filename, texts, tags, values, textIdx, fileLen):
             hasTag.extend(tag)
             hasValue.extend(value)
 
+    textCharToken = textCharTokenize(textStr)
+    textWordToken = textWordTokenize(textStr)
+    if textCharToken.shape[0] != textWordToken.shape[0]:
+        print("ERROR: textCharToken != textWordToken")
+
     for t in config.tagName:
+        tagCharToken = tagCharTokenize(t)
+        tagWordToken = tagWordTokenize(t)
+        if tagCharToken.shape[0] != tagWordToken.shape[0]:
+            print("ERROR: tagCharToken != tagWordToken")
+
         if t in hasTag:
             index = [idx for idx, tag in enumerate(hasTag) if tag == t]
             all_start, all_end = [], []
@@ -101,66 +145,13 @@ def setText(filename, texts, tags, values, textIdx, fileLen):
                 all_end.append(start + len(v))
             val_start = min(all_start)
             val_end = max(all_end)
-            textToken = textTokenize(textStr)
-            tagToken = tagTokenize(t)
-            textData.append(initText(filename, textToken, tagToken,
+
+            textData.append(initText(filename, textCharToken, tagCharToken, textWordToken, tagWordToken,
                                      val_start + 1, val_end + 1, 1, textIdx, index_bound, fileLen, textStr, t))
         else:
-            textToken = textTokenize(textStr)
-            tagToken = tagTokenize(t)
-            textData.append(initText(filename, textToken,
-                                     tagToken, -1, -1, 0, textIdx, index_bound, fileLen, textStr, t))
+            textData.append(initText(filename, textCharToken, tagCharToken, textWordToken,
+                                     tagWordToken, -1, -1, 0, textIdx, index_bound, fileLen, textStr, t))
     return textData
-
-
-# def preprocess(path, files):
-#     textData = []
-#     for file in tqdm(files, desc='Preprocessing', dynamic_ncols=True):
-#         df = pd.read_excel(path + file)
-
-#         text = df['Text'].tolist()
-#         tags = df['Tag'].tolist()
-#         values = df['Value'].tolist()
-#         textIdx = df['Index'].tolist()
-#         parIdx = df['Parent Index'].tolist()
-#         isTitle = df['Is Title'].tolist()
-#         newText = [removeSpace(t) for t in text]
-#         if '入札公告' in newText:
-#             title = newText.index('入札公告') + 1
-#         elif '入札公告（再度公告）' in newText:
-#             title = newText.index('入札公告（再度公告）') + 1
-#         else:
-#             title = 1
-
-#         # find the index of each title
-#         titleIdx = df[df['Parent Index'] == title].index.tolist()
-#         titleIdx.append(df.shape[0])
-
-#         beginTextIdx = []
-#         for i in range(10):
-#             if parIdx[i] == title and isTitle[i] != 'x':
-#                 beginTextIdx.append(i)
-
-#         start = min(beginTextIdx)
-#         end = max(beginTextIdx)
-#         if start < end:
-#             textData.extend(
-#                 setText(file, text[start:end], tags[start:end], values[start:end]))
-
-#         for i in range(len(titleIdx) - 1):
-#             curTitle = titleIdx[i]
-#             nextTitle = titleIdx[i + 1]
-#             secTitleIdx = textIdx[curTitle]
-#             # find the index of second title
-#             textBound = df[df['Parent Index'] == secTitleIdx].index.tolist()
-#             textBound.append(nextTitle)
-
-#             for i in range(len(textBound) - 1):
-#                 start, end = textBound[i:i+2]
-#                 if start < end:
-#                     textData.extend(setText(
-#                         file, text[start:end], tags[start:end], values[start:end]))
-#     return textData
 
 
 def findParent(idx, textIdx, parIdx, root):
@@ -208,7 +199,6 @@ def preprocess_new(path, files):
 
 
 if __name__ == "__main__":
-    # textData = preprocess(config.train_path, config.train_files)
     config.train_files.sort()
     textData = preprocess_new(config.train_path, config.train_files)
     with open("textData_train.pkl", 'wb') as f:
